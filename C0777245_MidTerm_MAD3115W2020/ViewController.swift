e0a470e070e848ee87202e6ee7cd720fec0b7c78
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-06 06:20:13 -0500

    Initial Commit

---
+++ C0777245_MidTerm_MAD3115W2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C0777245_MidTerm_MAD3115W2020
+//
+//  Created by Anmol singh on 2020-03-06.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
