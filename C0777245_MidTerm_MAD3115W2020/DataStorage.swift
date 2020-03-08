4d36db4e608ac17f13d4ca8c00b3b404d8ca6337
Anmol Singh <anmolsingh1597@gmail.com>
2020-03-08 15:02:33 -0400

    [Anmol Singh]Data: Data fetched from firebase and viewed as tableView successfully

--- C0777245_MidTerm_MAD3115W2020/DataStorage.swift
+++ C0777245_MidTerm_MAD3115W2020/DataStorage.swift
@@ -7,8 +7,11 @@
 //
 
 import Foundation
+import Firebase
+
 class DataStorage
 {
+    var ref = Database.database().reference()
     private static let instance = DataStorage()
     private lazy var customerList: [Customer] = []
     private init (){}
@@ -25,7 +28,17 @@ class DataStorage
         return self.customerList
     }
     func loadData(){
-        customerList.append(Customer.init(customerId: "gfh", firstName: "Me", lastName: "Me", emailId: "me@me", mobile: "8997"))
+        let refer = self.ref.child("Users")
+        refer.observeSingleEvent(of: .value, with: {(snapshot)
+            in
+            if let userDict = snapshot.value as? [String: [String: String]]{
+                for value in userDict.values{
+                    self.customerList.append((Customer.init(customerId: value["id"]!, firstName: value["firstName"]!, lastName: value["lastName"]!, emailId: value["email"]!, mobile: value["mobile"]!)))
+                }
+            }
+        })
+        
+        //customerList.append(Customer.init(customerId: "gfh", firstName: "Me", lastName: "Me", emailId: "me@me", mobile: "8997"))
 //        countryList.append(Country.init(name: "India", capital: "Afghanistan", flag: #imageLiteral(resourceName: "elephant.png")))
 //        countryList.append(Country.init(name: "Canada", capital: "Afghanistan", flag: #imageLiteral(resourceName: "elephant.png")))
 //        countryList.append(Country.init(name: "Afghanistan", capital: "Afghanistan", flag: #imageLiteral(resourceName: "elephant.png")))
@@ -34,4 +47,25 @@ class DataStorage
 //        countryList.append(Country.init(name: "Afghanistan", capital: "Afghanistan", flag: #imageLiteral(resourceName: "elephant.png")))
                                                   
     }
+    
+//
+//    func fetchDataFromFirebase() -> Array<String>{
+//        let refer = self.ref.child("Users") // this is refernce (it first check "Users" tree exist or not)
+//        refer.observeSingleEvent(of: .value, with: { (snapshot) in
+//        if let userDict = snapshot.value as? [String: [String: String]]{
+//            print("------------------------------------")
+//            //print(userDict)
+//            for value in userDict.values
+//            {
+//               // print(value["firstName"]!)
+//               // print(value["lastName"]!)
+//                    self.firstNameArray.append(value["firstName"]!)
+//            }
+//            dump(self.firstNameArray)
+//            //self.readData(users: userDict)
+//            //print(userDict.count)
+//            }})
+//        return self.firstNameArray
+//    }
+//
 }
